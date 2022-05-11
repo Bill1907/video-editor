9f034b10bd2f52b3f83da8694b01db4e2c20b678
boseong94 <boseong.kim@catenoid.net>
2022-05-11 22:25:45 +0900

    set up react player src

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -4,7 +4,11 @@ import ReactPlayer from 'react-player'
 const Video = () => {
 
   return <div>
-    <ReactPlayer/>
+    <ReactPlayer
+      className='react-player'
+      url={'https://www.youtube.com/watch?v=6xAAObSyTdM'}
+      controls={true}
+    />
   </div>
 };
 
