8c35e047e97862b65a0643b82b2fe06e99d70e57
boseong94 <boseong.kim@catenoid.net>
2022-05-15 21:53:50 +0900

    video js 설정

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.scss';
-import Video from '../Video/Video';
+import VideoPlayer from '../Video/Video';
+
+const videoJsOptions = {
+  sources: [
+    {
+      src: "//vjs.zencdn.net/v/oceans.mp4",
+      type: "video/mp4"
+    }
+  ]
+};
 
 function App() {
   return (
     <div className="App">
-      <Video/>
+      <VideoPlayer options={videoJsOptions}/>
     </div>
   );
 }
